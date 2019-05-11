fb9683c3fc522f757a6d0fa854e37680e89e6c27
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-11 15:00:57 -0500

    Set up game controller

--- Tic-Tac-Toe/Assets/Scripts/GameController.cs
+++ Tic-Tac-Toe/Assets/Scripts/GameController.cs
@@ -8,5 +8,28 @@ public class GameController : MonoBehaviour
     // Buttons added to the buttonList by dragging and dropping the Text fields for all 9 buttons into Game Controller Button List
     public Text[] buttonList;
 
+    void Awake()
+    {
+        SetGameControllerReferenceOnButtons();
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+
+    }
+
+    public string GetPlayerSide()
+    {
+        return "?";
+    }
+
+    public void EndTurn()
+    {
+        Debug.Log("EndTurn is not implemented!");
+    }
 
 }
