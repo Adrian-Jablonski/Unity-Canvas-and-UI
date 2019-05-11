6143e33e034d42b1679beaef617e51078014f01b
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-11 13:02:58 -0500

    Added Game Controller and linked buttons to button list

---
+++ Tic-Tac-Toe/Assets/Scripts/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    // Buttons added to the buttonList by dragging and dropping the Text fields for all 9 buttons into Game Controller Button List
+    public Text[] buttonList;
+
+
+}
