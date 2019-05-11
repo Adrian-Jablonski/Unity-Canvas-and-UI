bdc7b683f0cba0774449e747db4a438162268f4e
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-11 08:32:00 -0500

    Added button, button animation, and click event to scene

---
+++ Unity UI/Assets/Scripts/onclick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onclick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ClickTest()
+    {
+        Debug.Log("clicked");
+    }
+
+    public void ClickTest2(string text)
+    {
+        Debug.Log(text);
+    }
+}
