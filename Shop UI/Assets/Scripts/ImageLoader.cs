58207f166b9c76899f3058c1aaa4f49b7e495213
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-12 08:58:15 -0500

    Started list project and loaded image from URL

---
+++ Shop UI/Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ImageLoader : MonoBehaviour
+{
+    public string url = "http://lorempixel.com/400/200/nature";
+    public RawImage image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetTexture());
+    }
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            image.texture = myTexture;
+        }
+    }
+
+}
