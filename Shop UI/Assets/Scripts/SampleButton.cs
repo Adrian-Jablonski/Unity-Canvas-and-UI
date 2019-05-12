96d5c6b39887110c4a3bf5f64629907e4e2974b0
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-12 10:11:22 -0500

    Added button prefab and linked all fields to prefab variables

---
+++ Shop UI/Assets/Scripts/SampleButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour
+{
+
+    // Add references to these variables in the samplebutton created. Then scroll to the top the the SampleButton inspector and click Overrides in Prefab section and apply all changes. This will apply the changes to the prefab
+    public Button button;
+    public Text nameLabel;
+    public Text priceLabel;
+    public RawImage iconImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Setup()
+    {
+
+    }
+    
+}
